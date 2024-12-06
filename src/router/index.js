76f8901b8ddbe705f274ed54d9820e1884155483
olagunju-oluwabukola <olagunjuoluwabukola2018@gmail.com>
2024-12-06 17:32:03 +0100

    home

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      ccomponent: () => import('../Views/HomeView.vue'),
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   ccomponent: () => import('../Views/HomeView.vue'),
+    // },
     {
       path: '/about',
       name: 'About',
